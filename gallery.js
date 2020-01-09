7e21ca6ad2333908f37728c96d244626b3f8485b
Jay <jason_milton@hotmail.com>
2020-01-09 16:42:05 -0500

    can use arrow keys inside modal to change images, can only use tab outside modal for now. Enter key when using tab works

--- gallery.js
+++ gallery.js
@@ -21,6 +21,7 @@ function Gallery(gallery) {
     // Event listeners to be bound when we open the modal
     window.addEventListener('keyup', handleKeyUp);
     nextButton.addEventListener('click', showNextImage);
+    prevButton.addEventListener('click', showPrevImage);
   }
 
   function closeModal() {
@@ -28,6 +29,7 @@ function Gallery(gallery) {
     // TODO: add event listeners for clicks and keyboard
     window.removeEventListener('keyup', handleKeyUp);
     nextButton.removeEventListener('click', showNextImage);
+    prevButton.removeEventListener('click', showPrevImage);
   }
 
   function handleClickOutside(e) {
@@ -37,11 +39,16 @@ function Gallery(gallery) {
   }
 
   function handleKeyUp(event) {
-    if (event.key === 'Escape') closeModal();
+    if (event.key === 'Escape') return closeModal();
+    if (event.key === 'ArrowRight') return showNextImage();
+    if (event.key === 'ArrowLeft') return showPrevImage();
   }
 
   function showNextImage() {
     showImage(currentImage.nextElementSibling || gallery.firstElementChild);
+  }
+  function showPrevImage() {
+    showImage(currentImage.previousElementSibling || gallery.lastElementChild);
   }
 
   function showImage(el) {
@@ -62,6 +69,18 @@ function Gallery(gallery) {
   images.forEach(image =>
     image.addEventListener('click', e => showImage(e.currentTarget))
   );
+
+  // Loop over each image
+  images.forEach(image => {
+    // attach an event listener for each image
+    image.addEventListener('keyup', e => {
+      // When that is keyup'd, check if it was enter
+      if (e.key === 'Enter') {
+        // if it was enter, show that image
+        showImage(e.currentTarget);
+      }
+    });
+  });
 
   modal.addEventListener('click', handleClickOutside);
 }
